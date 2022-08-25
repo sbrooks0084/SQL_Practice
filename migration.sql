dadea8e888b1f2098a3605544536833d8b4796b5
Shannon Brooks <sbrooks0084@gmail.com>
2022-08-25 16:11:52 -0500

    added awards table

---
+++ migration.sql
@@ -0,0 +1,17 @@
+CREATE TABLE movies (
+  id serial,
+  title text,
+  duration integer,
+  rating varchar(10),
+  genre text,
+  is_3d boolean NOT NULL,
+  released_at timestamp with time zone,
+  score numeric(3, 1)
+);
+
+CREATE TABLE awards (
+    id serial,
+    movie_id serial,
+    kind text,
+    name text
+)
